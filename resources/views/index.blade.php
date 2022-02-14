cd6453bd51c2bce0809292756bb59b89a035c89b
dns2012 <dnsprogress@gmail.com>
2022-02-14 21:28:08 +0700

    add back button

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -40,6 +40,7 @@
         </div>
         @else
         <div class="row">
+            <div class="col-md-12 mb-3"><a href="/" class="btn btn-success"><< Halaman Utama</a></div>
             <div class="col-md-12">
                 <h4>Daftar Tamu <b>{{ $bride }}</b></h4>
                 <hr>
